efff09d1c48b0a28e28ea8df9803083c62bb665c
Huzaifa Nagariya <huzaifaali578@gmail.com>
2024-12-14 16:11:15 +0530

    fixing

--- services/commen.js
+++ services/commen.js
@@ -12,7 +12,5 @@ export const cookieExtractor = function(req) {
     if (req && req.cookies) {
         token = req.cookies['jwt'];
     }
-    // TODO: after testing we will remove this token
-    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NWE5NDUyOTcwZWEyYjM1YWNkZTQ1NiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTczNDExODg3Nn0.M0iYJPKvgHZ6hxKKJvxGTUTWTaaWDEti33MaNiQ_uJM"
     return token;
 };
